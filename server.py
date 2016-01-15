47e401db2b526e309a2c11995d6cfca887cdb2e3
James <ljm@puffgogo.com>
2016-01-15 18:07:45 +0800

    modify server

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import os
 import model
 import numpy as np
 
-import theano.sandbox.cuda
+#import theano.sandbox.cuda
 import string
 import random
 import feature_operation
@@ -14,7 +14,7 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 def tcplink(sock, addr):
     try:
-        theano.sandbox.cuda.use('cpu')
+        #theano.sandbox.cuda.use('cpu')
         ext = '.wav'
         print('Accept new connection from %s:%s...' % addr)
         while True:
@@ -22,19 +22,22 @@ def tcplink(sock, addr):
             data = data.strip()
             print(data)
             #if not data or os.path.splitext(data)[-1] != ext:
-#        if os.path.splitext(data)[-1] != ext:
+            assert(os.path.splitext(data)[-1] == ext)
 #            print data
 #            print os.path.splitext(data)[-1]
 #            sock.send(('File extention should be %s. Connection closed!'%ext))
 #            sock.close()
 #            return
+            st = time.time()
             tmpmfc = '/tmp/%s.mfc.tmp'%id_generator()
-            os.system('HCopy -c %s %s %s'%('wav_confi',data,tmpmfc))
+            wav_file = os.path.join(file_dir,data.strip())
+            os.system('HCopy -c %s %s %s'%('wav_confi',wav_file,tmpmfc))
             x,m = feature_operation.read_htk(tmpmfc)
             print 'read feature'
             val_predictions ,final_prediction = mdl.predict(x,m)
             print val_predictions
             sock.send((' %s' % final_prediction.decode('utf-8')).encode('utf-8'))
+            print '%ds passed'%time.time()-st
             os.system('rm %s'%tmpmfc)
     except BaseException,e:
         raise(e)
@@ -42,6 +45,7 @@ def tcplink(sock, addr):
         sock.close()
         print('Connection from %s:%s closed.' % addr)
 
+file_dir = '/home/james/audio'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('',9527))
 s.listen(5)
