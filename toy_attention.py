c80661afc6e65725ff87527b7facfb3d1cbfa32a
James Liu <james.liu.n1@gmail.com>
2016-04-08 15:19:18 +0800

    add gradient

--- toy_attention.py
+++ toy_attention.py
@@ -7,26 +7,24 @@ N_FEAT_X = 2
 N_FEAT_U = 1
 x =T.matrix('input')
 u =T.matrix('input2')
-target = T.ivector('target')
+target = T.matrix('target')
+#target = T.ivector('target')
 
 layers = {'input_x':lasagne.layers.InputLayer((None,N_FEAT_X))}
 layers['input_u']=lasagne.layers.InputLayer((None,N_FEAT_U))
 layers['v']=lasagne.layers.DenseLayer(layers['input_u'],num_units = 1, nonlinearity=lasagne.nonlinearities.linear, W = lasagne.init.Constant(1.), b= lasagne.init.Constant(0.))
 layers['a']=lasagne.layers.NonlinearityLayer(layers['v'], nonlinearity=lasagne.nonlinearities.sigmoid)
-#layers['a']=lasagne.layers.DenseLayer(layers['input_u'],num_units = 1, nonlinearity=lasagne.nonlinearities.linear, W = lasagne.init.Constant(1.), b= lasagne.init.Constant(0.))
 weights = T.tile(lasagne.layers.get_output(layers['a'], u ), (1, N_FEAT_X))
 x_hat = weights * x
-weighted_input = theano.function([x,u],x_hat)
 compute_weights= theano.function([u],weights)
-#layers['output'] = lasagne.layers.
-#input_layer = lasagne.layers.InputLayer((None,N_FEAT_X))
-#nnet = lasagne.layers.DenseLayer(input_layer, num_units = 1,nonlinearity = lasagne.nonlinearities.sigmoid)
-#l_out = T.flatten(lasagne.layers.get_output(nnet,x))
-#params = lasagne.layers.get_all_params(nnet)
-#loss = T.mean(T.nnet.binary_crossentropy(l_out,target))
+weighted_input = theano.function([x,u],x_hat)
+
+params = lasagne.layers.get_all_params(layers['a'])
+loss = T.mean((x_hat.flatten()-target.flatten())**2)
+#loss = T.mean(T.nnet.binary_crossentropy(x_hat,target))
 #updates_sgd = lasagne.updates.sgd(loss,params, learning_rate = 0.1)
 #train = theano.function([x,target],updates = updates_sgd)
-#gradients = theano.function([x,target],T.grad(loss,params))
+gradients = theano.function([x,u,target],T.grad(loss,params))
 
 
 X = np.array([[1,2],[1,2],[1,3],[4,2],[4,1],[3,3]]).astype('float32')
@@ -35,4 +33,4 @@ print X
 print compute_weights(U)
 print weighted_input(X,U)
 #Y = np.array([1,1,1,0,0,0]).astype('int32')
-#gradients (X,Y)
+print gradients (X,U,X)
